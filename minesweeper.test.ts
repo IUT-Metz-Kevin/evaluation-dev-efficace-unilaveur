3da69c11ae2985dbf5774afb596cf5b134d692f9
Maxime <e53428u@iutm-inf-f33-25.iutmetz.site.univ-lorraine.fr>
2025-06-03 14:09:40 +0200

    rouge + vert 2

--- minesweeper.test.ts
+++ minesweeper.test.ts
@@ -1,11 +1,17 @@
 import { assertEquals, assertThrows } from "jsr:@std/assert";
 
 function minesweeper(input: string): string {
-    return "0";
+    return input === "*" ? "*" : "0";
 }
 
 Deno.test("1x1 sans mine", () => {
     const input = ".";
     const expected = "0";
+    assertEquals(minesweeper(input), expected);
+});
+
+Deno.test("1x1 avec mine", () => {
+    const input = "*";
+    const expected = "*";
     assertEquals(minesweeper(input), expected);
 });
